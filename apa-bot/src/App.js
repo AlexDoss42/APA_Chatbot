5edc8d60c3fcffc8a31fba1083be77c9fc5609dc
AlexDoss42 <alex.doss42@gmail.com>
2019-08-19 13:10:01 -0600

    Adds code from docs to  app.js

--- apa-bot/src/App.js
+++ apa-bot/src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Pusher from 'pusher-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { PUSHER_APP_CLUSTER, PUSHER_APP_KEY } = process.env
 
-export default App;
+    class App extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          userMessage: '',
+          conversation: [],
+        };
+      }
+
+      componentDidMount() {
+        const pusher = new Pusher(`${PUSHER_APP_KEY}`, {
+          cluster: `${PUSHER_APP_CLUSTER}`,
+          encrypted: true,
+        });
+
+        const channel = pusher.subscribe('bot');
+        channel.bind('bot-response', data => {
+          const msg = {
+            text: data.message,
+            user: 'ai',
+          };
+          this.setState({
+            conversation: [...this.state.conversation, msg],
+          });
+        });
+      }
+
+      handleChange = event => {
+        this.setState({ userMessage: event.target.value });
+      };
+
+      handleSubmit = event => {
+        event.preventDefault();
+        if (!this.state.userMessage.trim()) return;
+
+        const msg = {
+          text: this.state.userMessage,
+          user: 'human',
+        };
+
+        this.setState({
+          conversation: [...this.state.conversation, msg],
+        });
+
+        fetch('http://localhost:5000/chat', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            message: this.state.userMessage,
+          }),
+        });
+
+        this.setState({ userMessage: '' });
+      };
+
+      render() {
+        const ChatBubble = (text, i, className) => {
+          return (
+            <div key={`${className}-${i}`} className={`${className} chat-bubble`}>
+              <span className="chat-content">{text}</span>
+            </div>
+          );
+        };
+
+        const chat = this.state.conversation.map((e, index) =>
+          ChatBubble(e.text, index, e.user)
+        );
+
+        return (
+          <div>
+            <h1>APA Citation Chatbot</h1>
+            <div className="chat-window">
+              <div className="conversation-view">{chat}</div>
+              <div className="message-box">
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                    value={this.state.userMessage}
+                    onInput={this.handleChange}
+                    className="text-input"
+                    type="text"
+                    autoFocus
+                    placeholder="Type your message and hit Enter to send"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+
+    export default App;
